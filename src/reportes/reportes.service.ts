455de66728e63a6e6f9d7f02c86c3039d5170b53
Jose Alejandro <44279311+josecd@users.noreply.github.com>
2023-07-27 15:26:42 -0500

    test html

--- src/reportes/reportes.service.ts
+++ src/reportes/reportes.service.ts
@@ -19,7 +19,14 @@ import { User } from 'src/users/entitiys/user.entity';
 import { ObservacionComentario } from 'src/observaciones/entitys/observacion-comentario.entity';
 
 import { createPdf } from '@saemhco/nestjs-html-pdf';
-import * as path from 'path';
+
+const fs = require('fs')
+const path = require('path')
+const utils = require('util')
+const puppeteer = require('puppeteer')
+const hb = require('handlebars')
+const readFile = utils.promisify(fs.readFile)
+
 @Injectable()
 export class ReportesService {
 
@@ -242,5 +249,45 @@ export class ReportesService {
     
     return createPdf(filePath, options, data);
   }
+  
+  async  getTemplateHtml(info:any) {
+    const data = info;
+
+    console.log("Loading template file in memory")
+    try {
+    const invoicePath = path.join(process.cwd(), 'templates', 'index.html');
+    return await readFile(invoicePath, 'utf8');
+    } catch (err) {
+    return Promise.reject("Could not load html template");
+    }
+    }
+
+  generatePdf(indo:any) {
+    let data = {};
+    this.getTemplateHtml(indo).then(async (res) => {
+    // Now we have the html code of our template in res object
+    // you can check by logging it on console
+    // console.log(res)
+    console.log(res);
+    
+    console.log("Compiing the template with handlebars")
+    const template = hb.compile(res, { strict: true });
+    // we have compile our code with handlebars
+    const result = template(data);
+    // We can use this to add dyamic data to our handlebas template at run time from database or API as per need. you can read the official doc to learn more https://handlebarsjs.com/
+    const html = result;
+    // we are using headless mode
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage()
+    // We set the page content as the generated html by handlebars
+    await page.setContent(html)
+    // We use pdf function to generate the pdf in the same folder as this file.
+    await page.pdf({ path: 'invoice.pdf', format: 'A4' })
+    await browser.close();
+    console.log("PDF Generated")
+    }).catch(err => {
+    console.error(err)
+    });
+    }
 
 }
