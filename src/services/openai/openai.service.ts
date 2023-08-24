48c46fd44c4d0218f614d3c5f39589f8790ed553
Jose Alejandro <44279311+josecd@users.noreply.github.com>
2023-08-23 19:44:14 -0500

    openia

--- src/services/openai/openai.service.ts
+++ src/services/openai/openai.service.ts
@@ -5,18 +5,18 @@ import * as  process from "process";
 @Injectable()
 export class OpenaiService {
 
- 
+
     // openai = new OpenAIApi(this.configuration)
-    private readonly openai:OpenAIApi
-    constructor(){
+    private readonly openai: OpenAIApi
+    constructor() {
         const configuration = new Configuration({
             apiKey: process.env.API_OPEN,
-            organization:'org-5F6cEvWbXdDPnR5D2XMjezjV'
+            organization: 'org-5F6cEvWbXdDPnR5D2XMjezjV'
         })
         this.openai = new OpenAIApi(configuration);
     }
-    async correccionGramatical(info:any) {
-        try{
+    async correccionGramatical(info: any) {
+        try {
             const response = await this.openai.createCompletion({
                 model: "text-davinci-003",
                 prompt: `corrige gramaticalmente el siguiente texto ${info}`,
@@ -25,13 +25,14 @@ export class OpenaiService {
                 top_p: 1,
                 frequency_penalty: 0,
                 presence_penalty: 0,
-              });
-                return await    response['data']['choices'][0]['text'].slice(2, -1) 
-        }
-     
-        catch(err){
-            console.log(err);
+            });
+            console.log(response['data']['choices'][0]['text']);
             
+            return await response['data']['choices'][0]['text'].slice(1).split("\n").join("")
+        }
+        catch (err) {
+            console.log(err);
+
         }
     }
 }
