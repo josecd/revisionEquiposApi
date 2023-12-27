c92cf652f5af9ba5a3cd799dff55ea43abeec598
Jose Alejandro <44279311+josecd@users.noreply.github.com>
2023-12-26 19:40:02 -0500

    test

--- src/reportes/reportes.service.ts
+++ src/reportes/reportes.service.ts
@@ -636,7 +636,6 @@ export class ReportesService {
   async generatepdfHtml2(info: any, tipo: any) {
     try {
       const browser = await puppeteer.launch({
-        headless: "new",
         executablePath: process.env.CHROMIUM_PATH,
         args: [
           '--no-sandbox',
