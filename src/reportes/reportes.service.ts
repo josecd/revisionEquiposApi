1834b0b6e2a3a5442d6908e17a2c3911f90d68dc
Jose Alejandro <44279311+josecd@users.noreply.github.com>
2024-02-19 23:03:02 -0500

    cambio

--- src/reportes/reportes.service.ts
+++ src/reportes/reportes.service.ts
@@ -636,7 +636,7 @@ export class ReportesService {
   async generatepdfHtml2(info: any, tipo: any) {
     try {
       const browser = await puppeteer.launch({
-        executablePath: "/usr/bin/chromium-browser",
+        executablePath: process.env.CHROMIUM_PATH,
         args: [
           '--no-sandbox',
           '--disable-setuid-sandbox',
